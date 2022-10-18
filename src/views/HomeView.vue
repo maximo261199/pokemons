a921efd6cba01f93634c859dfd4d744cc3d61c1c
maximo261199 <u201920341@upc.edu.pe>
2022-10-18 08:51:53 -0500

    piniav2.0

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import {useCounterStore} from "@/store/counter";
+import {storeToRefs} from "pinia";
+
+const useCounter = useCounterStore();
+const {increment} = useCounter;
+const {count, double}=storeToRefs(useCounter);
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+<h1>home counter:{{count}}</h1>
+<h2>Double:{{double}}</h2>
+  <button @click="increment">Increment</button>
 </template>
